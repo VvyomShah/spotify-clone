c87c4a6253c4f1bbc1937d5cff2e2b790babe257
Vvyom Shah <vvyomms@gmail.com>
2024-03-29 16:18:48 -0400

    Add login functions

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import Login from "./components/Login";
+import { useStateProvider } from "./utils/StateProvider";
+import { reducerCases } from "./utils/constants";
+import Spotify from "./components/Spotify";
+
+function App() {
+  const [ { token }, dispatch] = useStateProvider();
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split('&')[0].split('=')[1];
+      dispatch({action: reducerCases.SET_TOKEN, token})
+    }
+  }, [token, dispatch])
+
+
+  return (
+    <div>
+      {
+        token ? <Spotify /> : <Login />
+      }
+    </div>
+  );
+}
+
+export default App;
